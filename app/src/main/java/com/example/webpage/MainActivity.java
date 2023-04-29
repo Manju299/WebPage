51f14843496324440e8f73b7b7cd226edc0e59af
Manjunath.Hosamani <manjunathhosamani67@gmail.com>
2023-04-30 00:35:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webpage/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.webpage;
+
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://jioevents.com");
+        WebSettings webSettings=myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        getSupportActionBar().hide();
+    }
+    @Override
+    public void onBackPressed(){
+        if(myWebView.canGoBack()){
+            myWebView.goBack();
+        }
+        else{
+            finish();
+        }
+
+    }
+}
